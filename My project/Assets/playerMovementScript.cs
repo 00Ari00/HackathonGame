3c7d48720c81baab7331b5bfdec26ed738f7b694
00Ari00 <arissahe3485@gmail.com>
2025-04-08 12:11:55 -0400

    More movement

--- My project/Assets/playerMovementScript.cs
+++ My project/Assets/playerMovementScript.cs
@@ -7,15 +7,13 @@ public class playerMovementScript : MonoBehaviour
     public Rigidbody2D playerRB;
     public float moveSpeed = 5;
     public float velocity;
+    public float drag = 0.9f;
+    public bool grounded;
     private Vector2 moveDirection;
-    private float moveX;
-    private float moveY;
-
     // Update is called once per frame
     void Update()
     {
         ProcessInputs();
-        // Move();
 
     }
 
@@ -25,14 +23,12 @@ public class playerMovementScript : MonoBehaviour
     }
 
     void ProcessInputs() {
-        moveX = Input.GetAxis("Horizontal");
-        moveY = Input.GetAxis("Vertical");
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
 
         moveDirection = new Vector2(moveX, moveY).normalized;
         // using .normalized to ensure diagonal movement is not faster than vertical and horizontal movement
-    }
 
-    void Move() {
         if (Mathf.Abs(moveDirection.x) > 0.1) {
             playerRB.linearVelocity = new Vector2(moveDirection.x * moveSpeed, playerRB.linearVelocity.y);
         }
@@ -40,5 +36,17 @@ public class playerMovementScript : MonoBehaviour
         if (Math.Abs(moveDirection.y) > 0.1) {
             playerRB.linearVelocity = new Vector2(playerRB.linearVelocity.x, moveDirection.y * moveSpeed);
         }
+    }
+
+    void Move() {
+        checkGround();
+
+        if (grounded) {
+            playerRB.linearVelocity *= drag;
+        }
+    }
+
+    void checkGround() {
+        
     }
 }
