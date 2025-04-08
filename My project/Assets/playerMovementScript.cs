d9aadefcf65df262abea8f0c64ea021854f7282c
00Ari00 <arissahe3485@gmail.com>
2025-04-08 11:05:36 -0400

    Movement basic

--- My project/Assets/playerMovementScript.cs
+++ My project/Assets/playerMovementScript.cs
@@ -2,17 +2,34 @@ using UnityEngine;
 
 public class playerMovementScript : MonoBehaviour
 {
-    public float speed = 5;
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+
+    public Rigidbody2D playerRB;
+    public float moveSpeed = 5;
+    public float velocity;
+    private Vector2 moveDirection;
 
     // Update is called once per frame
     void Update()
     {
-        
+        ProcessInputs();
+        // Move();
+
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void ProcessInputs() {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        // using .normalized to ensure diagonal movement is not faster than vertical and horizontal movement
+        moveDirection = new Vector2(moveX, moveY).normalized;
+    }
+
+    void Move() {
+        playerRB.linearVelocity = moveDirection * moveSpeed;
     }
 }
